7bee072cfbaee6ebf8fcc34e9b123a34923b6560
建中貓貓男 <54308739+MaowMan@users.noreply.github.com>
2020-03-31 23:13:43 +0800

    2020/3/31

---
+++ src/JS-Ti-82/main/app.js
@@ -0,0 +1,33 @@
+function init() {
+    $(":button").attr('onclick', "Button_pressed(this);");
+}
+
+function Button_pressed(button) {
+    $(".mainview").attr("value", String($(".mainview").val()) + String(button.value))
+    Check_view();
+}
+
+function Check_view() {
+    let content = $(".mainview").val();
+    let last = content.slice(-1);
+    if (last === "l") {
+        if (content.length == 3) {
+            content = new String();
+        } else {
+            content = content.slice(0, -4);
+        }
+    } else if (last === "C") {
+        content = new String();
+    } else if (last === "=") {
+        try {
+            content = content.slice(0, -1);
+            let result = 0;
+            eval("result=".concat(content));
+            content = String(result);
+        } catch (error) {
+            alert(error);
+            content = new String()
+        }
+    }
+    $(".mainview").attr("value", content);
+}
